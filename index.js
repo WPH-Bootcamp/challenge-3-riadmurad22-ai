a2a10de372b4b7b0a04ffbfad6d98e058e05032a
riadmurad22-ai <riadmurad22@gmail.com>
2025-11-12 15:31:37 +0700

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // code here, goodluck!!
+
+let prompt = require("prompt-sync")();
+
+function getValidNumberInput(prompMessage) {
+  let num;
+  let flag = 0;
+  do {
+    flag++;
+    if (flag > 1)
+      console.log("Input tidak valid, silakan masukkan angka yang benar.");
+    num = prompt(prompMessage);
+  } while (isNaN(num) || num.trim() === "");
+  return parseFloat(num);
+}
+
+function calculation(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    case "%":
+      return a % b;
+    case "**":
+      return a ** b;
+    default:
+      return "Operator tidak valid";
+  }
+}
+
+console.log("Program Calculator Sederhana");
+
+while (true) {
+  let angka = getValidNumberInput("Masukan angka: ");
+  console.log("typeof angka:", typeof angka); // typeof check number type
+
+  let angka2 = getValidNumberInput("Masukan angka kedua: ");
+
+  let operator = prompt("Masukan operator (+, -, *, /, %, **): ");
+
+  // store the result of calculation function
+  let hasil = calculation(parseInt(angka), parseInt(angka2), operator);
+
+  console.log(
+    "Hasil calculator :",
+    angka,
+    " ",
+    operator,
+    " ",
+    angka2,
+    " = ",
+    hasil
+  );
+
+  let question = prompt("Apakah anda ingin lanjut? (y/n): ");
+
+  if (question.toLowerCase() === "n") {
+    console.log("Terima kasih telah menggunakan kalkulator sederhana ini.");
+    break;
+  }
+}
+console.log("Program selesai.");
---
+++ trial.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+let prompt = require("prompt-sync")();
+let n = prompt("Masukkan angka: ");
+console.log("Angka yang dimasukkan adalah: ", n);
+let nama=prompt("Masukkan nama: ");
+let alamat=prompt("Masukkan alamat: ");
+let umur=prompt("Masukkan umur: ");
+console.log("Nama saya ", nama, ", alamat saya di ", alamat, ", umur saya ", umur, " tahun.");
+
